a64166f74880dcbc48b943f13bf8f3a3ce900df4
LOKRANJAN19 <lokranjanpremkumar392@gmail.com>
2025-04-26 21:23:02 +0530

    created Procfile and static

--- Project/settings.py
+++ Project/settings.py
@@ -123,7 +123,11 @@ STATICFILES_DIRS = [
     
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ALLOWED_HOSTS = ['your-app-name.onrender.com']
